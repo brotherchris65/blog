2b27c584f3dd97c91fe4e80515e06f7bda7594dd
Chris <brotherchris65@gmail.com>
2020-11-19 18:40:20 -0600

    new user page and static file handle by whitenoise

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('blog.urls')),
 ]
